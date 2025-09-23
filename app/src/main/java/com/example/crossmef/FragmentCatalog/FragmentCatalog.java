3fce85f38f6fc5f57cf264e948cd666e1c27c821
Aleksandr <froteru955@gmail.com>
2025-09-23 22:17:02 +0300

    Create Fragment v0.4

---
+++ app/src/main/java/com/example/crossmef/FragmentCatalog/FragmentCatalog.java
@@ -0,0 +1,22 @@
+package com.example.crossmef.FragmentCatalog;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.crossmef.R;
+
+
+public class FragmentCatalog extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_catalog, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/crossmef/FragmentUserProfile/FragmentUserProfile.java
@@ -0,0 +1,21 @@
+package com.example.crossmef.FragmentUserProfile;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.crossmef.R;
+
+public class FragmentUserProfile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        return view;
+    }
+}
